342058de4a88a25d9aecb3579dc944533e0af499
Dan Magid <dan97mm@gmail.com>
2024-09-17 22:10:09 +0300

    Initial commit

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 800;
+const port = 8000;
 
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
-// Route all requests to the index.html file
-app.get('*', (req, res) => {
+// Route for the login page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// Route for the dashboard (main page with map)
+app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Redirect root to login
+app.get('/', (req, res) => {
+  res.redirect('/login');
 });
 
 app.listen(port, () => {
